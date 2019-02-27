6bfb3f959f32d7f98010fe130df1318066ccbe8b
Paulo Nascimento <paulormnas@gmail.com>
2019-02-27 10:24:21 -0300

    Make web page available by node http module and make Vue.js examples

--- app.js
+++ app.js
@@ -1,41 +1,40 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const fs = require('fs')
+const app = require('http')
+const port = 8080
+const ip = 'localhost'
+const index = 'public/index.html'
+const index_css = 'public/stylesheet/index.css'
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+server = app.createServer((req, res) => {
+    console.log('Recebendo request \n' + req.url)
+    res.writeHead(200)
 
-var app = express();
+    if(req.url === '/'){
+        fs.readFile(index,'utf8', (err, data) => {
+            if (err)
+                throw err
+            else {
+                res.end(data)
+            }
+        })
+    }
+    else{
+        fs.readFile('public' + req.url,'utf8', (err, data) => {
+            if (err)
+                throw err
+            else {
+                res.end(data)
+            }
+        })
+    }
+})
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+server.listen(port, ip, () => {
+    console.log('Servidor de pé em ' + ip + ':' + port)
+})
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+// app.get('public/index.html', (res) =>{
+//     console.log(res)
+// }).on('error', (e) => {
+//     console.error(`Got error: ${e.message}`)
+// })

6269502db3e20781585b050ef07dc5988c68ea4a
Paulo Nascimento <paulormnas@gmail.com>
2019-03-15 19:07:59 -0300

    começando a entender como usar o express e criar rotas

--- app.js
+++ app.js
@@ -1,47 +1,66 @@
+let express = require('express')
+let app = express()
+let engines = require('consolidate')
 const fs = require('fs')
-const app = require('http')
 const port = 8080
-const ip = '192.168.0.13'           // atualizar o ip antes de iniciar o servidor
-const index = 'public/index.html'
-const email_page = 'public/email.html'
+const ip = '0.0.0.0'           // atualizar o ip antes de iniciar o servidor
+const index = 'index.html'
+const email_page = 'email.html'
 const index_css = 'public/stylesheet/index.css'
 
-server = app.createServer((req, res) => {
-    console.log('Recebendo request \n' + req.url)
-    res.writeHead(200)
+app.use(express.static(__dirname + '/public'))
+app.set('views', __dirname + '/public')
+app.engine('html', engines.mustache)
+app.set('view engine', 'html')
 
-    if(req.url === '/'){
-        fs.readFile(index, (err, data) => {
-            if (err)
-                throw err
-            else {
-                res.end(data)
-            }
-        })
-    }
-    else if(req.url === '/email'){
-        fs.readFile(email_page, (err, data) => {
-            if (err)
-                throw err
-            else {
-                res.end(data)
-            }
-        })
-    }
-    else{
-        fs.readFile('public' + req.url, (err, data) => {
-            if (err)
-                throw err
-            else {
-                res.end(data)
-            }
-        })
-    }
+app.get('/', (req, res) => {
+	res.render(index)
 })
 
-server.listen(port, ip, () => {
-    console.log('Servidor de pé em ' + ip + ':' + port)
+app.get('/email', (req, res) => {
+	res.render(email)
 })
+
+app.listen(port, ip, () => {
+	console.log('Servidor de pé em ' + ip + ':' + port)
+})
+
+// server = app.createServer((req, res) => {
+//     console.log('Recebendo request \n' + req.url)
+//     res.writeHead(200)
+// 
+//     if(req.url === '/'){
+//         fs.readFile(index, (err, data) => {
+//             if (err)
+//                 throw err
+//             else {
+//                 res.end(data)
+//             }
+//         })
+//     }
+//     else if(req.url === '/email'){
+//         fs.readFile(email_page, (err, data) => {
+//             if (err)
+//                 throw err
+//             else {
+//                 res.end(data)
+//             }
+//         })
+//     }
+//     else{
+//         fs.readFile('public' + req.url, (err, data) => {
+//             if (err)
+//                 throw err
+//             else {
+//                 res.end(data)
+//            }
+//         })
+//    }
+// })
+
+// server.listen(port, ip, () => {
+//     console.log('Servidor de pé em ' + ip + ':' + port)
+// })
 
 // app.get('public/index.html', (res) =>{
 //     console.log(res)
